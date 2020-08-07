ed24c87510c9295d29352ea2ef903c783a700e39
ibbocus <ib_bocus@hotmail.com>
2020-08-07 15:17:40 +0100

    small change to readme

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
   scp -i ~/.ssh/DevOpsStudents.pem -r ~/Downloads/NodeJS-AWS-Deploy/starter-code/app/ ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com:/home/ubuntu/
-  ssh -i ~/.ssh/DevOpsStudents.pem ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com -i $ mkdir environment
   scp -i ~/.ssh/DevOpsStudents.pem -r ~/Downloads/NodeJS-AWS-Deploy/starter-code/environment/app ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com:/home/ubuntu/environment
   ssh -i ~/.ssh/DevOpsStudents.pem ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com -i $ ./environment/app/provisions.sh
 
