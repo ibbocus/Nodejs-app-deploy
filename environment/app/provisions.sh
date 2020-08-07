d01af49fdbe0f1f13de80a5d96cebe1efd84a406
ibbocus <ib_bocus@hotmail.com>
2020-08-07 15:51:07 +0100

    Mongod db provision setup

--- environment/app/provisions.sh
+++ environment/app/provisions.sh
@@ -9,7 +9,7 @@
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
   sudo apt-get install -y nodejs
   sudo apt-get install npm -y
-
+  sudo npm install ejs, mongoose, express
 # Configuring nginx proxy
   IP=$(curl ifconfig.me)
 
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
   scp -i ~/.ssh/DevOpsStudents.pem -r ~/Downloads/NodeJS-AWS-Deploy/starter-code/app/ ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com:/home/ubuntu/
   scp -i ~/.ssh/DevOpsStudents.pem -r ~/Downloads/NodeJS-AWS-Deploy/starter-code/environment/app ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com:/home/ubuntu/environment
+  scp -i ~/.ssh/DevOpsStudents.pem -r ~/Downloads/NodeJS-AWS-Deploy/starter-code/environment/db ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com:/home/ubuntu/environment
+  ssh -i ~/.ssh/DevOpsStudents.pem ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com -i $ ./environment/db/provisions.sh
   ssh -i ~/.ssh/DevOpsStudents.pem ubuntu@ec2-52-212-156-251.eu-west-1.compute.amazonaws.com -i $ ./environment/app/provisions.sh
-
 
